17a4416d5b9cc2a81149de83a9e8dbddefb20304
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-05-28 14:57:25 +0600

    Finished the application, added styles and file .editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,63 @@
-import './App.css'
+import React, {useState} from 'react';
+import './App.css';
+import TriesCounter from './components/TriesCounter';
+import ResetButton from './components/ResetButton';
+import GameField from './components/GameField';
 
-function App() {
-
-  return (
-    <>
-    </>
-  )
+interface Props {
+  hasItem: boolean;
+  clicked: boolean;
 }
 
-export default App
+const createItems = (): Props[] => {
+  const cells: Props[] = [];
+  for (let i = 0; i < 36; i++) {
+    cells.push({
+      hasItem: false,
+      clicked: false
+    });
+  }
+
+  const randomCell = Math.floor(Math.random() * cells.length);
+  cells[randomCell].hasItem = true;
+
+  return cells;
+};
+
+const App: React.FC = () => {
+  const [items, setItems] = useState(createItems());
+  const [tries, setTries] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+
+  const OnReset = () => {
+    setItems(createItems());
+    setTries(0);
+    setGameOver(false);
+  };
+
+  const OnItemClick = (index: number) => {
+    if (gameOver || items[index].clicked) return;
+
+    const copyItems = items.map((item, i) =>
+      i === index ? {...item, clicked: true} : item
+    );
+
+    setItems(copyItems);
+    setTries(tries + 1);
+
+    if (copyItems[index].hasItem) {
+      setGameOver(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <GameField items={items} onItemClick={OnItemClick} />
+      {gameOver && <div className="GameOver">Item found! Tries: {tries}</div>}
+      <TriesCounter tries={tries} />
+      <ResetButton onReset={OnReset} />
+    </div>
+  );
+};
+
+export default App;
